d6602c9b03887e881abeb810aa6a5c0df5e60895
Whatisinternet <joshteeter@gmail.com>
2014-12-11 00:04:15 -0500

    Attempting to make sure geocoder is working

--- models/therapist.rb
+++ models/therapist.rb
@@ -3,11 +3,17 @@ require "geocoder/railtie"
 Geocoder::Railtie.insert
 
 class Therapist < ActiveRecord::Base
+	extend Geocoder::Model::ActiveRecord
 	geocoded_by :postal_code
 
 	def find_therapists(postal_code, distance, units)
 		return [] if postal_code.nil?
-		valid_therapists = Therapist.near(postal_code, distance.to_i, :units => units.to_sym)
+		if units.downcase.to_s == 'km'
+			valid_therapists = Therapist.near(postal_code, distance.to_i, :units => :km)
+		else
+			valid_therapists = Therapist.near(postal_code, distance.to_i, :units => :mi)
+		end
+		
 	end
 
 	def return_all
