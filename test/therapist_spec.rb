09ab0e90f20c2ce89620ba34debe253eda2e299c
Whatisinternet <joshteeter@gmail.com>
2014-12-12 18:37:41 -0500

    Spec changes. Testing for secondary condtion

--- test/therapist_spec.rb
+++ test/therapist_spec.rb
@@ -32,6 +32,18 @@ describe "Therapist" do
 		check_therapists.must_include 'longitude'
 	end
 
+		it "should return local therapists" do
+		check_therapists = Therapist.new.find_therapists('90210',1000,'mi').refine.to_json
+		therapists = [{id: 1, name: "Test Therapist", location: "123 Fake St. NY NY 90210", postal_code: "90210", phone_number: "555-555-5555", fax_number: "555-555-5555", email: "test@therapist.com", website: "api.helpfinder.com", latitude: 34.1030032, longitude: -118.4104684, distance: 0.0, bearing:"0.0"}]
+		check_therapists.must_equal therapists.to_json
+		check_therapists.must_include 'name'
+		check_therapists.must_include 'location'
+		check_therapists.must_include 'website'
+		check_therapists.must_include 'phone_number'
+		check_therapists.must_include 'latitude'
+		check_therapists.must_include 'longitude'
+	end
+
 	it "should create a new therapist" do
 		temp = Therapist.new(name: 'Test',
 			location: '123 Fake St. NY NY 90210',
@@ -60,6 +72,8 @@ describe "Therapist" do
 		temp2.must_be_instance_of Therapist
 		temp.destroy
 		temp2.destroy
+		temp2 = Therapist.find_by_name('Test')
+		temp2.must_be_nil
 	end
 
 	it "should delete an entry from therapist" do
