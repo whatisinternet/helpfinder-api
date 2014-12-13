85f1662917ef3e7f2d9766b2b36d598927766a3f
Whatisinternet <joshteeter@gmail.com>
2014-12-12 19:01:42 -0500

    Updated test

--- test/help_finder_spec.rb
+++ test/help_finder_spec.rb
@@ -20,8 +20,7 @@ describe "Help Finder" do
 	it "should return a json package for local therapists" do
 		get 'api/1/find_help/90210/20/km'
 		therapists = [{id: 1, name: "Test Therapist", location: "123 Fake St. NY NY 90210", postal_code: "90210", phone_number: "555-555-5555", fax_number: "555-555-5555", email: "test@therapist.com", website: "api.helpfinder.com", latitude: 34.1030032, longitude: -118.4104684, distance: 0.0, bearing:"0.0"}]
-		last_response.body.must_equal therapists.to_json
-
+		last_response.body.must_include therapists.to_json
 	end
 
 	it "should return json" do
